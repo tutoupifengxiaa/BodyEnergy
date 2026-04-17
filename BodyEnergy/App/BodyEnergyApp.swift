0e0b237c314f4da1fbd323c43e1ff073ee85b93a
An <752511101@qq.com>
2026-04-17 21:15:44 +0800

    Remove fixed screen bounds from app root layout

--- BodyEnergy/App/BodyEnergyApp.swift
+++ BodyEnergy/App/BodyEnergyApp.swift
@@ -1,24 +1,16 @@
-﻿import SwiftUI
+import SwiftUI
 
 @main
 struct BodyEnergyApp: App {
     @StateObject private var store = AppStore.preview
-    private let screenBounds = UIScreen.main.bounds
 
     var body: some Scene {
         WindowGroup {
-            ZStack {
-                Color.white
-                    .ignoresSafeArea()
-
-                ContentView()
-                    .frame(width: screenBounds.width, height: screenBounds.height)
-            }
-            .frame(width: screenBounds.width, height: screenBounds.height)
-            .environmentObject(store)
-            .task {
-                await store.refreshHealthData()
-            }
+            ContentView()
+                .environmentObject(store)
+                .task {
+                    await store.refreshHealthData()
+                }
         }
     }
 }
